739ebba007258626c640ff42555ce26feb5683f1
Tameem A <tameem.daradkah@gmail.com>
2025-09-14 13:18:17 -0700

    Initialize server with essential files and dependencies
    - Created package.json with dependencies: cors, dotenv, express, nodemon, pg
    - Added initial structure with empty files for database client, index, AI logic, authentication middleware, and routes for AI, applications, and authentication.

---
+++ client/tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {},
+  },
+  darkMode: "class", // optional (remove if you don’t want dark mode toggle)
+  plugins: [],
+};
